77585530bdadad02e8456caf31f4175440e42ccf
BritoAlv <gomzalezbritoalvaro@gmail.com>
2022-07-26 16:20:56 -0400

    Add files via upload

--- Insertion/insertion/insertion.cs
+++ Insertion/insertion/insertion.cs
@@ -1,7 +1,7 @@
 ﻿// implementations of insertion sort.
 public static class insertion_sort
 {
-    public static void sort_swaps_0(int[] source)
+    public static void swaps_0(int[] source)
     /* 
     The thing is that inside the while there are three constant operations.
     */
@@ -20,35 +20,47 @@ public static class insertion_sort
             }
         }
     }
-    public static void sort_swaps_1(int[] read, int[] insert)
-    /* this sort is designed to benchmark, the thing about benchmarking is that we take an array as example, and we call the function sort a lot of times with that array (average execution time), but if the function works with the same array, first time function will sort the array, but after that, the function will work with the same sorted array, so recommended solution https://stackoverflow.com/questions/73108219/confused-about-complexity-of-insertion-sort-and-benchmark?noredirect=1#comment129120562_73108219 is read from original array and insert in other array, this will not destroy the original array.
-    */ 
-    {
-        for (int i = 0; i < read.Length; i++)
-        {
-            insert[i] = read[i]; // this increase constant time only.
-            int j = i;
-            // the array from position 0 to j-1 is sorted always, so now insert
-            // source[i] in that array, keeping it sorted.
-            while((j > 0) && (read[j-1] > read[j]) ) 
-            // has to go from i-1 to 0, checking, in each check, it compares and after that do the swap.
-            {                
-                int temp = read[j-1];
-                read[j-1] = read[j];
-                read[j] = temp;
-                j--;
-            }
 
-        }
-    }
-    public static void sort_binary_search_0(int[] source)
+    public static void binary_search_0(int[] source)
     {
         for (int i = 1; i < source.Length; i++) // n-1 iterations, so O(n)
         {
             // the array from position 0 to j-1 is sorted always, so now insert source[i] in that array, keeping it sorted.
             // binary search to insert source[i] in correct position.
             // find index where to insert, and after that shift indexes.
-            
+            int lookup = source[i];
+            int left = 0;
+            int right = i-1;
+            int mid = 0;
+            int pos = 0;
+            while(left < right)
+            {
+                mid = (left+right)/2;
+                if(source[mid] == lookup)
+                {
+                    pos = mid;
+                    break;
+                }
+                else if(source[mid] < lookup)
+                {
+                    left = mid+1;
+                }
+                else 
+                {
+                    right = mid-1;
+                }
+                pos = right;    
+            }
+            if (lookup > source[pos])
+            // this happens inserting 15 into 2 3 6 8
+            {
+                pos = pos+1;
+            }
+            for (int j = i; j >= pos+1; j--)
+            {
+                source[j] = source[j-1];
+            }
+            source[pos] = lookup;
         }        
     }
 
@@ -57,10 +69,10 @@ public static class insertion_sort
 public class insertion{
     public static void Main()
     {
-        int[] test_sort = new int[10] {1,3,2,52,4,3,5,6,4,6};
+        int[] test_sort = new int[10] {1,2,2,2,4,3,90,90,91,600};
         int[] sorted = new int[10];
-        //insertion_sort.sort_swaps_0(test_sort);
-        insertion_sort.sort_swaps_1(test_sort, sorted);
+        //insertion_sort.binary_search_0(test_sort);
+        //insertion_sort.binary_search_0(test_sort, sorted);
         foreach (int num in test_sort)
         {
             Console.Write(" " + num + " ");
