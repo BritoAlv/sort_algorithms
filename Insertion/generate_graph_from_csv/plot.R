aedb93e251a2edc0821cd5737dca0209b9f13de1
BritoAlv <gomzalezbrito@gmail.com>
2022-07-28 00:49:43 -0400

    Add code to generate_the_graph.

---
+++ Insertion/generate_graph_from_csv/plot.R
@@ -0,0 +1,9 @@
+data <- read.csv("benchmark-report-swap_1.csv")
+plot(type="b", x = data$Tamaño, y = data$Microsegundos, col="red", xlab="tiempo", ylab= "tamaño", xlim = c(0,50000), ylim= c(0,50000) )
+data2 <- read.csv("benchmark-report-binary_search_1.csv")
+lines(type="b", x=data2$Tamaño, y = data2$Microsegundos, col="blue")
+legend(20000,40000,c("swap","binary search"), lwd=c(5,2), col=c("red","blue"), pch=c(15,19), y.intersp=1.5)
+title("Insertion Sort O(n²)")
+dev.print(svg, file ="plot.svg")
+
+
