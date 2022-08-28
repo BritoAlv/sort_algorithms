6f8f6801cd5b8bd25d674d40f538857c3be4b079
BritoAlv <gomzalezbrito@gmail.com>
2022-08-28 09:58:24 -0400

    Use List<int> instead of Array

--- sort_merge/Merge-Code/merge/merge.cs
+++ sort_merge/Merge-Code/merge/merge.cs
@@ -10,22 +10,19 @@
             basic_0(source, (start+end)/2+1, end );
 
             // join halves using two-finger-algorithm
-            int[] result = new int[end-start+1];
+            List<int> result = new List<int>(end-start+1);
             int finger1 = start;
             int finger2 = (start+end)/2+1;
-            int index = 0;
             while (finger1 <= (start+end)/2 && finger2 <= end)
             {
                 if(source[finger1] <  source[finger2])
                 {
-                    result[index] = source[finger1];
-                    index++;
+                    result.Add(source[finger1]);
                     finger1 = finger1 + 1;
                 }
                 else
                 {
-                    result[index] = source[finger2];
-                    index++;
+                    result.Add(source[finger2]);
                     finger2 = finger2 + 1;
                 }                
             }
@@ -33,14 +30,12 @@
             // when this process ends there are two options either finger1 = finger2 or finger2 = end but not both happen at the same time.
             while (finger1 <= (start+end)/2)
             {
-                result[index] = source[finger1];
-                index++;
+                result.Add(source[finger1]);
                 finger1 = finger1 + 1;
             }
             while (finger2 <= end)
             {
-                result[index] = source[finger2];
-                index++;
+                result.Add(source[finger2]);
                 finger2 = finger2 + 1;
             }
             for (int i = start; i <= end; i++)
@@ -54,7 +49,7 @@
 
     public static void Main()
     {
-        int[] test_sort = random_utils.generate_array(100);
+        int[] test_sort = random_utils.generate_array(1000);
         merge_sort.basic_0(test_sort, 0, test_sort.Length-1);
         foreach (int num in test_sort)
         {
